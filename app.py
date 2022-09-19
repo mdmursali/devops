6d8eba5dc377efeac3ec27804266538037b20015
mdmursali <112893169+mdmursali@users.noreply.github.com>
2022-09-19 17:48:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,11 @@
-from flask import *
-import os
-import socket
-
+from flask import Flask
 app = Flask(__name__)
 
-# app.config["Debug"] = True
 @app.route('/')
-@app.route('/home')
-def home():
-    return render_template('index.html')
-
-    
-if __name__ == '__main__':
-    app.run()
+def hello():
+	return "welcome to the flask tutorials"
 
 
-PORT = int(os.environ.get("PORT", 9090))
-if __name__ == '__main__':
-    app.run(threaded=True,host='0.0.0.0',port=PORT)
-
-if __name__ == "__main__":
-  app.run(host='0.0.0.0', port=9090)
+if _name_ == "__main__":
+	app.run(host ='0.0.0.0', port = 5001, debug = True
+            
